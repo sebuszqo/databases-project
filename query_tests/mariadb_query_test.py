3a07cc2abe7740e32a0897b76e8f40c1e114f8a0
remekozicki <remigiusz.kozicki@gmail.com>
2025-04-02 21:56:22 +0200

    mariadb_query_test - uncomment

--- query_tests/mariadb_query_test.py
+++ query_tests/mariadb_query_test.py
@@ -11,44 +11,21 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor(dictionary=True)
 
-# Wyszukanie klienta o client_id = 1
-# specific_client_transaction_number_query = """
-#     SELECT COUNT(t.transaction_id)
-#     FROM transactions AS t
-#     JOIN accounts AS a ON t.account_id = a.account_id
-#     JOIN clients AS c ON c.client_id = a.client_id
-#     WHERE c.client_id = %s
-#     AND t.transaction_type = 'credit';
-# """
-# s = time.time()
-# cursor.execute(specific_client_transaction_number_query, (1,))
-# e = time.time()
-# result = cursor.fetchone()
-# print(f"Liczba transakcji credytowych klienta o ID = 1 wynosi: {result}. Czas wykonanai operacji: {e-s}s")
 
-
-client_with_more_than_one_card_query = '''
-    SELECT c.client_id, c.first_name, c.last_name, COUNT(cr.card_id) AS card_count
-    FROM clients AS c
-    JOIN accounts AS a ON c.client_id = a.client_id
-    JOIN cards AS cr ON a.account_id = cr.account_id
-    GROUP BY c.client_id, c.first_name, c.last_name
-    HAVING COUNT(cr.card_id) > 1
-    LIMIT 10;
-'''
-
+specific_client_transaction_number_query = """
+    SELECT COUNT(t.transaction_id)
+    FROM transactions AS t
+    JOIN accounts AS a ON t.account_id = a.account_id
+    JOIN clients AS c ON c.client_id = a.client_id
+    WHERE c.client_id = %s
+    AND t.transaction_type = 'credit';
+"""
 s = time.time()
-cursor.execute(client_with_more_than_one_card_query)
+cursor.execute(specific_client_transaction_number_query, (1,))
 e = time.time()
+result = cursor.fetchone()
+print(f"Liczba transakcji credytowych klienta o ID = 1 wynosi: {result}. Czas wykonanai operacji: {e-s}s")
 
-result = cursor.fetchall()
-
-
-for row in result:
-    print(f"liczba transakcji credytowych klienta o ID = 1 wynosi: {row}.")
-
-
-print(f"Zapytanie wykonane w {e - s:.6f} sekundy")
 
 # Zamknięcie połączenia
 cursor.close()
