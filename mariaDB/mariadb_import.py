950b617aba4e897976376af74897dd9b3439a2f6
sebuszqo <sekn.kn@gmail.com>
2025-04-09 19:16:06 +0200

    import scripts for mariadb and postgresql

--- mariaDB/mariadb_import.py
+++ mariaDB/mariadb_import.py
@@ -1,39 +1,78 @@
-import pandas as pd
-from sqlalchemy import create_engine, text
+import pymysql
+import csv
 import time
+import statistics
 
-# Konfiguracja połączenia MariaDB
-MARIADB_CONN = "mysql+pymysql://user:pass@localhost:3306/testdb"
-mariadb_engine = create_engine(MARIADB_CONN)
-
-def insert_csv_to_db(engine, table_name, file_path):
-    start_time = time.time()
-    df = pd.read_csv(file_path)
-
-    # Przygotuj listę kolumn do inserta
-    columns = df.columns.tolist()
-    placeholders = ', '.join([':%s' % col for col in columns])
-    column_names = ', '.join([f"`{col}`" for col in columns])
-    insert_sql = f"INSERT INTO `{table_name}` ({column_names}) VALUES ({placeholders})"
-  
-    with engine.begin() as conn:  # użycie transakcji
-        for _, row in df.iterrows():
-            conn.execute(text(insert_sql), row.to_dict())
-
-    elapsed_time = time.time() - start_time
-    print(f"Dane z {file_path} zaimportowane do tabeli {table_name} w {elapsed_time:.2f} sekundy.")
-
-def import_mariadb_data():
-    data_files = {
-    "clients": "data/clients.csv",
-    "accounts": "data/accounts.csv",
-    "transactions": "data/transactions.csv",
-    "cards": "data/cards.csv",
-    "loans": "data/loans.csv",
+DB_CONFIG = {
+    'db': 'testdb',
+    'user': 'user',
+    'password': 'pass',
+    'host': 'localhost',
+    'port': 3306
 }
 
-    for table, file in data_files.items():
-        insert_csv_to_db(mariadb_engine, table, file)
+CSV_FILE = '../data/cards.csv'
+REPEAT_COUNT = 100
+TABLE_NAME = 'cards'
+
+
+def read_first_line_csv(file_path):
+    file = open(file_path, 'r')
+    first_line = file.readline()
+    split_line = first_line.split(",")
+    return split_line
+    
+    
+def read_csv(file_path):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return list(reader)
+
+def clear_table(conn):
+    with conn.cursor() as cur:
+        cur.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+    conn.commit()
+
+def insert_cards(conn, data):
+    with conn.cursor() as cur:
+        rows = [
+            (row['card_id'], row['account_id'], row['card_number'], row['expiry_date'])
+            for row in data
+        ]
+        
+        start = time.perf_counter()
+        # Batch INSERT – 1 zapytanie z wieloma VALUES
+        sql = f"INSERT INTO {TABLE_NAME} (card_id, account_id, card_number, expiry_date) VALUES (%s, %s, %s, %s)"
+        cur.executemany(sql, rows)
+        conn.commit()
+        end = time.perf_counter()
+
+    return len(rows), end - start
+
+def main():
+    split_line = read_first_line_csv(CSV_FILE)
+    print(split_line)
+    # print("📥 Wczytywanie danych...")
+    # data = read_csv(CSV_FILE)
+    # print(f"🔢 Wczytano {len(data)} rekordów.")
+    # times = []
+
+    # for i in range(REPEAT_COUNT):
+    #     print(f"▶️  Iteracja {i+1}/{REPEAT_COUNT}...")
+
+    #     conn = pymysql.connect(**DB_CONFIG)
+    #     clear_table(conn)
+    #     count, elapsed = insert_cards(conn, data)
+    #     times.append(elapsed)
+    #     conn.close()
+
+    #     print(f"⏱️  Czas: {elapsed:.4f} s")
+
+    # print("\n📊 Podsumowanie:")
+    # print(f"Średni czas: {statistics.mean(times):.4f} s")
+    # print(f"Minimalny czas: {min(times):.4f} s")
+    # print(f"Maksymalny czas: {max(times):.4f} s")
+    # print(f"Wszystkie czasy: {[round(t, 4) for t in times]}")
 
 if __name__ == "__main__":
-    import_mariadb_data()
+    main()
---
+++ postgreSQL/load_clients_postgreSQL.py
@@ -0,0 +1,75 @@
+import psycopg2
+from psycopg2.extras import execute_values
+import csv
+import time
+import statistics
+
+DB_CONFIG = {
+    'dbname': 'testdb',
+    'user': 'user',
+    'password': 'pass',
+    'host': 'localhost',
+    'port': 5432
+}
+
+CSV_FILE = '../data/clients.csv'
+REPEAT_COUNT = 1
+TABLE_NAME = 'clients'
+
+def read_csv(file_path):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return list(reader)
+
+def clear_table(conn):
+    cur = conn.cursor()
+    cur.execute(f'TRUNCATE TABLE {TABLE_NAME} RESTART IDENTITY CASCADE')
+    conn.commit()
+    cur.close()
+
+def insert_clients(conn, data):
+    cur = conn.cursor()
+    rows = [
+        (row['client_id'], row['first_name'], row['last_name'], row['email'])
+        for row in data
+    ]
+
+    start = time.perf_counter()
+    execute_values(
+        cur,
+        f"INSERT INTO {TABLE_NAME} (client_id, first_name, last_name, email) VALUES %s",
+        rows
+    )
+    conn.commit()
+    end = time.perf_counter()
+    cur.close()
+
+    return len(rows), end - start
+
+def main():
+    print("📥 Wczytywanie danych...")
+    data = read_csv(CSV_FILE)
+    print(f"🔢 Wczytano {len(data)} rekordów.")
+    times = []
+
+    for i in range(REPEAT_COUNT):
+        print(f"▶️  Iteracja {i+1}/{REPEAT_COUNT}...")
+
+        conn = psycopg2.connect(**DB_CONFIG)
+        clear_table(conn)
+        count, elapsed = insert_clients(conn, data)
+        times.append(elapsed)
+        conn.close()
+
+        print(f"⏱️  Czas: {elapsed:.4f} s")
+
+    
+    print("\n📊 Podsumowanie:")
+    print(f"Średni czas: {statistics.mean(times):.4f} s")
+    print(f"Minimalny czas: {min(times):.4f} s")
+    print(f"Maksymalny czas: {max(times):.4f} s")
+    print(f"Wszystkie czasy: {[round(t, 4) for t in times]}")
+
+
+if __name__ == "__main__":
+    main()
